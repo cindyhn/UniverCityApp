071b69f1cf5fca35d90cb96da4f17b3d61598555
Cindy <z5117324@student.unsw.edu.au>
2019-07-02 15:45:54 +1000

    Initial Commit

---
+++ app/src/main/java/com/example/univercityapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.univercityapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Sign up page
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

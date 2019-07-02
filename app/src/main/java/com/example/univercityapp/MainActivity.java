b1b43e8cd8cf1ddd6793785262dea2d66bfc2141
Cindy <z5117324@student.unsw.edu.au>
2019-07-02 15:46:53 +1000

    Initial Commit

--- app/src/main/java/com/example/univercityapp/MainActivity.java
+++ app/src/main/java/com/example/univercityapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.univercityapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//Sign up page
 
 public class MainActivity extends AppCompatActivity {
 

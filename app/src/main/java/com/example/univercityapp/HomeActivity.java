4d46991c7e782314cb9557907c06b0442800ef6f
vishalnomula <vishal.nomula.3@gmail.com>
2019-07-30 23:22:02 +1000

    Moved buttons to home page and re-directed to respective activities.

--- app/src/main/java/com/example/univercityapp/HomeActivity.java
+++ app/src/main/java/com/example/univercityapp/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.univercityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void launchMyWamScreen(View v) {
+        Intent myIntent = new Intent(getBaseContext(), MyWamActivity.class);
+        startActivity(myIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void launchMajorMapsScreen(View v) {
+        Intent myIntent = new Intent(getBaseContext(), MajorMapsActivity.class);
+        startActivity(myIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void launchQuizScreen(View v) {
+        Intent myIntent = new Intent(getBaseContext(), QuizActivity.class);
+        startActivity(myIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void launchHelpScreen(View v) {
+        Intent myIntent = new Intent(getBaseContext(), HelpActivity.class);
+        startActivity(myIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    public void launchExplorationScreen(View v) {
+        Intent myIntent = new Intent(getBaseContext(), ExplorationActivity.class);
+        startActivity(myIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 }

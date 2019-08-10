4b42f3cb4da2dbde482d67c125b564c21bf4c48c
Henry <henryzhao16@gmail.com>
2019-08-10 21:44:17 +1000

    Accounting Webview Test

---
+++ app/src/main/java/com/example/univercityapp/ExplorationAccounting.java
@@ -0,0 +1,33 @@
+package com.example.univercityapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ExplorationAccounting extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exploration_accounting);
+
+        webView = findViewById(R.id.myWebView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.loadUrl("https://www.handbook.unsw.edu.au/undergraduate/specialisations/2019/ACCTA1");
+    }
+
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/univercityapp/ExplorationActivity.java
+++ app/src/main/java/com/example/univercityapp/ExplorationActivity.java
@@ -1,11 +1,13 @@
 package com.example.univercityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.view.View;
 
-public class ExplorationActivity extends AppCompatActivity {
+public class ExplorationActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView tvExplorationMain;
 
@@ -20,6 +22,10 @@ public class ExplorationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exploration);
 
+        if (getIntent().getBooleanExtra("EXIT", false)) {
+            finish();
+        }
+
         btExplorationAccounting = findViewById(R.id.btExplorationAccounting);
         btExplorationBusinessEconomics = findViewById(R.id.btExplorationBusinessEconomics);
         btExplorationBusinessLaw = findViewById(R.id.btExplorationBusinessLaw);
@@ -31,8 +37,34 @@ public class ExplorationActivity extends AppCompatActivity {
 
         tvExplorationMain = findViewById(R.id.tvExplorationMain);
 
-
-
-
+        btExplorationAccounting.setOnClickListener(this);
     }
+
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = null;
+
+            switch (view.getId()) {
+                case R.id.btExplorationAccounting: intent = new Intent(getApplicationContext(), ExplorationAccounting.class);
+                    break;
+
+                default: break;
+            }
+
+            if (intent != null){
+                try {
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+
+                } catch (Exception e){
+                }
+
+            }
+        }
+
+
 }
+

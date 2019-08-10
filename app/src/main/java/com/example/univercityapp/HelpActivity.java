2061221a42f8d97509778ff24a1928bb6362a406
vishalnomula <vishal.nomula.3@gmail.com>
2019-08-10 19:19:03 +1000

    Help page cleaned up.

--- app/src/main/java/com/example/univercityapp/HelpActivity.java
+++ app/src/main/java/com/example/univercityapp/HelpActivity.java
@@ -1,7 +1,9 @@
 package com.example.univercityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+
+    public void launchHomeScreen(View v) {
+        Intent myIntent = new Intent(getBaseContext(), HomeActivity.class);
+        startActivity(myIntent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 
 }
